fc732510e2cb5e0e2596d4f2308c551b846bf295
Alejandro Cabezas U <alejandro.cabezas@firmadox.cl>
2023-03-28 17:01:35 -0300

    agregar ciudad a pais inyectando objeto

---
+++ springframework/src/main/java/com/acu/beans/Pais.java
@@ -0,0 +1,23 @@
+package com.acu.beans;
+
+public class Pais {
+	private String nombre;
+	private Ciudad ciudad;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
+}
--- springframework/src/main/java/com/acu/springframework/App.java
+++ springframework/src/main/java/com/acu/springframework/App.java
@@ -12,13 +12,15 @@ import com.acu.beans.Persona;
 
 public class App {
 
-	public static void main(String[] args) {		
-		//se define un contenedor para que spring lo pueda manejar(spring container)		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/acu/xml/beans.xml");	
-		//Patron de diseño factory donde pasamos un id y nos devuelve la isntancia de un objeto en particular
-		Persona per = (Persona) appContext.getBean("persona");		
-		System.out.println(per.getId()+ " " + per.getNombre()+" "+per.getApodo());
-		((ConfigurableApplicationContext)appContext).close();
+	public static void main(String[] args) {
+		// se define un contenedor para que spring lo pueda manejar(spring container)
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/acu/xml/beans.xml");
+		// Patron de diseño factory donde pasamos un id y nos devuelve la isntancia de
+		// un objeto en particular
+		Persona per = (Persona) appContext.getBean("persona");
+		System.out
+				.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " " + per.getPais().getNombre()+ " " + per.getPais().getCiudad().getNombre());
+		((ConfigurableApplicationContext) appContext).close();
 	}
 
 }
