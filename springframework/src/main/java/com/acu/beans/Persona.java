520029d6476744c54fa214fd135c6e7d5d69f16f
Alejandro Cabezas U <alejandro.cabezas@firmadox.cl>
2023-03-28 15:39:20 -0300

    agregar constructores para ocuparlos en bean

--- springframework/src/main/java/com/acu/beans/Persona.java
+++ springframework/src/main/java/com/acu/beans/Persona.java
@@ -6,34 +6,35 @@ public class Persona {
 	private String apodo;
 
 	public Persona(int id, String nombre, String apodo) {
-		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.apodo = apodo;
 	}
+	
+
+	public Persona(int id) {
+		this.id = id;
+	}
+
+
+	public Persona(String apodo) {
+		this.apodo = apodo;
+	}
+
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getNombre() {
 		return nombre;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+
 
 	public String getApodo() {
 		return apodo;
-	}
-
-	public void setApodo(String apodo) {
-		this.apodo = apodo;
 	}
 
 }
