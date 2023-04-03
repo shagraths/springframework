a30fb5083697eca862020cd179e7e4d66c26e563
Alejandro Cabezas U <alejandro.cabezas@firmadox.cl>
2023-04-03 13:18:45 -0400

    scan de bean por carpeta y crear estereotipo

--- springframework/src/main/java/com/acu/beans/Barcelona.java
+++ springframework/src/main/java/com/acu/beans/Barcelona.java
@@ -1,7 +1,13 @@
 package com.acu.beans;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 import com.acu.interfaces.IEquipo;
 
+@Component
 public class Barcelona implements IEquipo{
 
 	@Override
